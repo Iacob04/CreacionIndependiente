b60091cb1d10eb749e2e8e118c098072c1f23434
Iacob04 <alexiacob2004@gmail.com>
2026-05-20 13:00:52 +0200

    Corregido error De lectura del excel y está completo

--- AnalizadorArtesEscenicas/utils.js
+++ AnalizadorArtesEscenicas/utils.js
@@ -15,14 +15,22 @@ function leerExcel(file) {
         const data = new Uint8Array(e.target.result);
         const workbook = XLSX.read(data, { type: "array" });
 
-        // Primera hoja del libro
-        const nombreHoja = workbook.SheetNames[0];
-        const hoja = workbook.Sheets[nombreHoja];
+        let todasLasFilas = [];
 
-        // header: 1 → primera fila como cabecera, defval: "" → celdas vacías como string vacío
-        const filas = XLSX.utils.sheet_to_json(hoja, { defval: "" });
+        // Recorrer todas las hojas
+        workbook.SheetNames.forEach((nombreHoja) => {
+          const hoja = workbook.Sheets[nombreHoja];
 
-        resolve(filas);
+          const filas = XLSX.utils.sheet_to_json(hoja, {
+            defval: ""
+          });
+
+          // Añadir filas al array general
+          todasLasFilas = todasLasFilas.concat(filas);
+        });
+
+        resolve(todasLasFilas);
+
       } catch (err) {
         reject(err);
       }
